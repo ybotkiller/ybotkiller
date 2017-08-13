8909b98ed1b60e2c46aef85a4bfd76bb51a4b15b
Andrey Vasnetsov <vasnetsov93@gmail.com>
2017-08-13 16:07:00 +0300

    update bot estimation in base

--- update_bots.py
+++ update_bots.py
@@ -2,16 +2,16 @@ import pickle
 
 from elastic_api.es_doc_indexer import ESDocIndexer
 
-model = pickle.load(open("data/sentiment_model.pickle", 'rb'))
+model = pickle.load(open("./notebooks/model_hypercomments3.pkl", 'rb'))
 
 
 def sentiment_updater(record):
     # res = analyser.get_sentiment(record['comment']) // 2
 
-    neg, pos = model.predict_proba([record['comment']])[0]
+    not_bot_prob, bot_prob = model.predict_proba([record['comment']])[0]
     return {
-        "model_sentiment_pos": pos,
-        "model_sentiment_neg": neg
+        "not_bot_prob": not_bot_prob,
+        "bot_prob": bot_prob
     }
 
 
